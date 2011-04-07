1f33fec140c9772b061abcf3787048c675bb44ff
Alexander Plavin <chersanya@gmail.com>
2011-04-07 19:44:48 +0400

    Initial commit

---
+++ BatteryInfo.cs
@@ -0,0 +1,112 @@
+﻿
+using System;
+using System.Management;
+using System.Reflection;
+
+namespace BatMan
+{
+    static class BatteryInfo
+    {
+        // The fields are filled through Reflection, so it's OK when a warning is shown here
+        private static ManagementObject BatteryStatus;
+        private static ManagementObject BatteryFullChargedCapacity;
+        private static ManagementObject BatteryStaticData;
+
+        static BatteryInfo()
+        {
+            var scope = new ManagementScope("\\\\.\\root\\WMI");
+            scope.Connect();
+
+            foreach (string className in new string[] { "BatteryStatus", "BatteryFullChargedCapacity", "BatteryStaticData" })
+            {
+                var query = new ObjectQuery("SELECT * FROM " + className);
+                using (var searcher = new ManagementObjectSearcher(scope, query))
+                {
+                    foreach (ManagementObject queryObj in searcher.Get())
+                    {
+                        typeof(BatteryInfo).GetField(className, BindingFlags.NonPublic | BindingFlags.Static).SetValue(null, queryObj);
+                    }
+                }
+            }
+        }
+
+        #region Properties (WMI info)
+
+        // BatteryStatus class properties
+        public static bool PowerOnline
+        {
+            get { return (bool)BatteryStatus.GetPropertyValue(MethodBase.GetCurrentMethod().Name.Substring(4)); }
+        }
+        public static bool Charging
+        {
+            get { return (bool)BatteryStatus.GetPropertyValue(MethodBase.GetCurrentMethod().Name.Substring(4)); }
+        }
+        public static int ChargeRate
+        {
+            get { return (int)BatteryStatus.GetPropertyValue(MethodBase.GetCurrentMethod().Name.Substring(4)); }
+        }
+        public static bool Discharging
+        {
+            get { return (bool)BatteryStatus.GetPropertyValue(MethodBase.GetCurrentMethod().Name.Substring(4)); }
+        }
+        public static int DischargeRate
+        {
+            get { return (int)BatteryStatus.GetPropertyValue(MethodBase.GetCurrentMethod().Name.Substring(4)); }
+        }
+        public static uint RemainingCapacity
+        {
+            get { return (uint)BatteryStatus.GetPropertyValue(MethodBase.GetCurrentMethod().Name.Substring(4)); }
+        }
+        public static uint Voltage
+        {
+            get { return (uint)BatteryStatus.GetPropertyValue(MethodBase.GetCurrentMethod().Name.Substring(4)); }
+        }
+
+        // BatteryFullChargedCapacity class properties
+        public static uint FullChargedCapacity
+        {
+            get { return (uint)BatteryFullChargedCapacity.GetPropertyValue(MethodBase.GetCurrentMethod().Name.Substring(4)); }
+        }
+
+        // BatteryStaticData class properties
+        public static uint DesignedCapacity
+        {
+            get { return (uint)BatteryStaticData.GetPropertyValue(MethodBase.GetCurrentMethod().Name.Substring(4)); }
+        }
+        public static string DeviceName
+        {
+            get { return (string)BatteryStaticData.GetPropertyValue(MethodBase.GetCurrentMethod().Name.Substring(4)); }
+        }
+        public static string ManufactureName
+        {
+            get { return (string)BatteryStaticData.GetPropertyValue(MethodBase.GetCurrentMethod().Name.Substring(4)); }
+        }
+
+        #endregion
+
+        #region Properties (countable)
+
+        public static double WearPercents
+        {
+            get { return 100.0 * (DesignedCapacity - FullChargedCapacity) / DesignedCapacity; }
+        }
+
+        public static double Percents
+        {
+            get { return 100.0 * RemainingCapacity / FullChargedCapacity; }
+        }
+
+        public static TimeSpan DischargeTimeLeft
+        {
+            get { return Discharging ? TimeSpan.FromHours((double)RemainingCapacity / DischargeRate) : TimeSpan.Zero; }
+        }
+
+        public static TimeSpan ChargeTimeLeft
+        {
+            get { return Charging ? TimeSpan.FromHours((double)(FullChargedCapacity - RemainingCapacity) / ChargeRate) : TimeSpan.Zero; }
+        }
+
+        #endregion
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Shell;
+using System.Windows.Threading;
+
+namespace BatMan
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            if (TaskbarItemInfo == null)
+                TaskbarItemInfo = new TaskbarItemInfo();
+
+            DispatcherTimer dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(UpdateState);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Start();
+        }
+
+        private void UpdateState(object sender, EventArgs e)
+        {
+            TaskbarItemInfo.ProgressState = TaskbarItemProgressState.Normal;
+            TaskbarItemInfo.ProgressValue = new Random().NextDouble(); // BatteryInfo.Percents / 100
+
+            TaskbarItemInfo.Overlay = new BitmapImage(new Uri(BatteryInfo.PowerOnline ? "ac_power.png" : "battery.png", UriKind.Relative));
+        }
+
+    }
+}
